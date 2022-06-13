dfa810b8166dd999583366c255e3806191f0786a
fald <4348649+fald@users.noreply.github.com>
2022-06-12 23:57:53 -0400

    Agent: Main training function mostly implemented, apart from model. inclusion.

--- agent.py
+++ agent.py
@@ -26,7 +26,7 @@ class Agent:
     def train_long_memory(self):
         pass
     
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, game_over):
         pass
     
     def get_action(self, state):
@@ -34,7 +34,43 @@ class Agent:
 
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    
+    while True:
+        # Get old state
+        state_old = agent.get_state(game)
+        
+        # Get move based on the state
+        final_move = agent.get_action(state_old)
+        
+        # Perform the move and get the new state
+        rewards, game_over, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+        
+        # Train the short memory - one step
+        agent.train_short_memory(state_old, final_move, rewards, state_new, game_over)
+        
+        # Remember who you are
+        agent.remember(state_old, final_move, rewards, state_new, game_over)
+        
+        if game_over:
+            # train the long (or 'replay' or 'experienced replay') memory
+            # plot the results
+            game.reset()
+            agent.number_of_games += 1
+            agent.train_long_memory()
+            
+            if score > record:
+                record = score
+                # TODO: agents.model.save()
+            
+            print('Game: {} Score: {}\nRecord: {}'.format(agent.number_of_games, score, record))
+            # TODO: plot
 
 
 if __name__ == "__main__":
