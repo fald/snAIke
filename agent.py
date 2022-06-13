862f285b3f9df626dba710a49f01d0dc84fffc74
fald <4348649+fald@users.noreply.github.com>
2022-06-12 23:31:55 -0400

    Agent: Global vars in place, max mem, batch size, and learning rate.

--- agent.py
+++ agent.py
@@ -3,3 +3,7 @@ import random
 import numpy as np
 from collections import deque
 from snake_game_ai import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+ALPHA = 0.001 # Learning rate
