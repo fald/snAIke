7d2ee5653ca9736950d256cf81637eabfb48cc5b
fald <4348649+fald@users.noreply.github.com>
2022-06-12 23:45:53 -0400

    Agent: Added class skeleton.

--- agent.py
+++ agent.py
@@ -7,3 +7,32 @@ from snake_game_ai import SnakeGameAI, Direction, Point
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 ALPHA = 0.001 # Learning rate
+
+
+class Agent:
+    def __init__(self):
+        pass
+    
+    def get_state(self, game):
+        pass
+    
+    def remember(self, state, action, reward, next_state, game_over):
+        pass
+        
+    def train_long_memory(self):
+        pass
+    
+    def train_short_memory(self):
+        pass
+    
+    def get_action(self, state):
+        pass
+
+
+def train():
+    pass
+
+
+if __name__ == "__main__":
+    train()
+    
