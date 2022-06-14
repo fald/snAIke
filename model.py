d68030b2d0e4b27e6ff48db55d1802ac483400dd
fald <4348649+fald@users.noreply.github.com>
2022-06-14 15:29:55 -0400

    Model: Trainer init.

--- model.py
+++ model.py
@@ -20,4 +20,18 @@ class Linear_QNetwork(nn.Module):
         if not os.path.exists(model_folder_path):
             os.makedirs(model_folder_path)
         torch.save(self.state_dict(), os.path.join(model_folder_path, filename))
-            
+        
+    def load(self, filename="model.pth", model_folder_path="./model"):
+        pass
+    
+    
+class QTrainer:
+    def __init__(self, model, alpha=0.001, gamma=0.9, optimizer=optim.Adam, loss_fn=nn.MSELoss):
+        self.model = model
+        self.alpha = alpha
+        self.gamma = gamma
+        self.optimizer = optimizer(self.model.parameters(), lr=self.alpha)
+        # Loss function
+        self.criterion = loss_fn()
+        
+        
