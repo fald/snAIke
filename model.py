e0f4d6f4df4a11e9fd0fa96a15359709e1378f85
fald <4348649+fald@users.noreply.github.com>
2022-06-14 16:53:32 -0400

    Model: train_step; So much stuff I half (less?) understand.

--- model.py
+++ model.py
@@ -34,4 +34,45 @@ class QTrainer:
         # Loss function
         self.criterion = loss_fn()
         
+    def train_step(self, state, action, reward, next_state, done):
+        # Should be able to handle single or multiple inputs.
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.float)
+        reward = torch.tensor(reward, dtype=torch.float)
+        # If multiple inputs, size is (n, x) with n = batch size
+        # otherwise, size is (x)
         
+        if len(state.shape) == 1:
+            # Want to append a dimension, using torch.unsqueeze
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            # we also want to convert the single done value to a tuple
+            done = (done, )
+            
+        # 1. Predicted Q-vals with curr state
+        prediction = self.model(state)
+        
+        # 2. Q_new = R + gamma * max(Next predicted Q val)
+        # clone the predictions, then get the argmax of the action to get the correct index (we want our action noted, not the 3-tuple)
+        # Only if not done, otherwise we want the reward
+        # Bellman eqn stuff from theory - though apparently I'm retarded.
+        target = prediction.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new += self.gamma * torch.max(self.model(next_state[idx]))
+                
+            target[idx][torch.argmax(action).item()] = Q_new
+            
+        # Apply loss function
+        # Empty gradient
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, prediction)
+        loss.backward() # apply backprop and gradients
+        
+        self.optimizer.step()
+        
+    
