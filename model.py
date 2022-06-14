8b515e1bf9679f545413a7c170aee3e25871c8dd
fald <4348649+fald@users.noreply.github.com>
2022-06-14 15:11:27 -0400

    Model: init layers and the forward method.

--- model.py
+++ model.py
@@ -3,3 +3,16 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
+
+
+class Linear_QNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x) # No need for an activation function here, just use the raw value.
+        return x
+
